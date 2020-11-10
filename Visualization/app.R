9a68c4f7d6a78c9f95947641ace5c8be9d8b9487
Qi Hua <qihua@qis-macbook-pro.lan>
2020-11-10 00:20:24 -0500

    visualization

--- Visualization/app.R
+++ Visualization/app.R
@@ -2,6 +2,8 @@ library(shiny)
 library(tidyverse)
 library(grid)
 library(png)
+library(Unicode)
+
 forestfire<-read.csv("forestfires.csv")
 map <- readPNG("map.png")
 
@@ -9,7 +11,7 @@ map <- readPNG("map.png")
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("month","Month:",1,12,1,step = 1)
+      sliderInput("month","Month:",1,12,1,step = 1,ticks = FALSE)
     ), #closing sidebarPanel
     mainPanel(
       plotOutput("map")
@@ -25,10 +27,13 @@ server <- function(input, output) {
     forestfire %>%
       filter(month==list[input$month]) %>%
       ggplot(aes(X, Y))+
-      annotation_custom(rasterGrob(map))+
-      geom_jitter()+
-      scale_x_continuous(breaks = seq(0, 9),limits=c(1,9))+
-      scale_y_reverse(breaks = seq(0, 9),limits=c(9,1))
+      annotation_custom(rasterGrob(map,width = unit(1,"npc"), 
+                                   height = unit(1,"npc")), 
+                        xmin = 0.5, xmax = 9.2, ymin = -9.2, ymax = -0.5)+
+      geom_jitter(width = 0.25,height = 0.25)+
+      scale_x_continuous(breaks = seq(1, 9),limits=c(0.5,9.5))+
+      scale_y_reverse(breaks = seq(1, 9),limits=c(9.5,0.5))+
+      theme_void()
   })
 }
 
